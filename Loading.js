31d56e3e688cafce7718ed012a4127519fdee7ab
hyohwan <hhseo79@gmail.com>
2020-10-20 00:09:19 +0900

    ~ #13

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the fucking weather...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fdf6aa',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+  },
+  text: {
+    color: '#2c2c2c',
+    fontSize: 30,
+  },
+});
